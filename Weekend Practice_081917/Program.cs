dedab723fa5f82db0f16abd81ff2cf0ea39b1569
bradberger01 <bradberger@hotmail.com>
2017-08-19 16:49:53 -0400

    Second Commit

--- Weekend Practice_081917/Program.cs
+++ Weekend Practice_081917/Program.cs
@@ -9,7 +9,29 @@ namespace Weekend_Practice_081917
     class Program
     {
         static void Main(string[] args)
-        {
+        {// If/Than Conditionals: Age restriction 
+         //int userAge;
+         //Console.WriteLine("Please enter Your age");
+         //userAge = int.Parse(Console.ReadLine());
+
+            //if(userAge >= 17)
+            //{ Console.WriteLine("you are " + userAge + ", and are old enough to see this movie."); }
+            //else
+            //{ Console.WriteLine("you are " + userAge + ", and are not old enough to see this movie."); }
+
+            // Asking for name and giving a response
+            Console.WriteLine("What is your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + ". It is nice to meet you.");
+
+            
+                         
+
+
+
+
+
+
         }
     }
 }
